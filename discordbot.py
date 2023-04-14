fffabdb90ec4e868805881f6035f69ddca634fbe
sopo9880 <66225600+sopo9880@users.noreply.github.com>
2023-04-14 13:49:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,4 @@
-import discord, openai, os, time, random, asyncio
+import discord, openai, os, time, random, asyncio, requests
 from discord.ext import commands
 from cmath import log
 from distutils.sysconfig import PREFIX
@@ -9,6 +9,7 @@ intents = discord.Intents.all()
 client = commands.Bot(command_prefix='*')
 
 openai.api_key = "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"
+api_key = RIOT_API
 
 #=============================================================
 
@@ -133,9 +134,265 @@ async def 사용자지연시간(ctx):
 
 #=============================================================
 
+#챔피언 이름 들고오는 함수
+def get_champion_name(champion_id):
+    response = requests.get(f"http://ddragon.leagueoflegends.com/cdn/{get_version()}/data/ko_KR/champion.json")
+    response_json = response.json()
+    champion_name = response_json['data'][champion_id]['name']
+    return champion_name
+
+# 소환사명으로부터 puuid를 가져오는 함수
+def get_puuid(summoner_name):
+    url = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_key}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data['puuid']
+    else:
+        return None
+
+
+#게임 모드의 한국어 반환
+def get_game_mode(queue_id: int) -> str:
+    game_modes = {
+        400: '일반',
+        420: '솔로랭크',
+        430: '일반',
+        440: '자유 5:5 랭크',
+        450: '무작위 총력전',
+        700: '소환사의 협곡 5:5 랭크',
+        710: '일반',
+        720: '우르프(URF)',
+        830: '솔로 랭크 칼바람 나락',
+        900: '모두 무작위 U.R.F.',
+        920: '칼바람 나락',
+        1900: '선택 U.R.F.'
+    }
+    if queue_id in game_modes:
+        return game_modes[queue_id]
+    else:
+        return "알 수 없는 게임 모드입니다."
+
+#게임 스펠 한국어
+def get_spell_name(summoner_spell_id):
+    url = f'https://kr.api.riotgames.com/lol/static-data/v4/summoner-spells/{summoner_spell_id}?locale=ko_KR&api_key={api_key}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['name']
+    else:
+        return 'Unknown'
+
+#게임 룬 한국어
+def get_primary_perk(participant):
+    primary_style = participant['perks']['styles'][0]
+    for selection in primary_style['selections']:
+        if selection['perk'] != 0:
+            return selection['perk']
+    return None
+
+def get_sub_perk_style(participant):
+    sub_style = participant['perks']['styles'][1]
+    return sub_style['style']
+
+def get_rune_name(rune_id):
+    # 룬 정보를 담을 딕셔너리를 생성합니다.
+    RUNE_DICT = {}
+
+    RUNE_URL = f"https://kr.api.riotgames.com/lol/static-data/v4/runes?locale=ko_KR&api_key={api_key}"
+    rune_data = requests.get(RUNE_URL).json()
+
+    for rune_id in rune_data["data"].keys():
+        rune_name = rune_data["data"][rune_id]["name"]
+        RUNE_DICT[int(rune_id)] = rune_name
+
+    print(RUNE_DICT[rune_id])
+    return RUNE_DICT[rune_id]
+
+def get_primary_perk(participant):
+    primary_style = participant['perks']['styles'][0]
+    for selection in primary_style['selections']:
+        if selection['perk'] != 0:
+            return selection['perk']
+    return None
+
+def get_sub_perk_style(participant):
+    sub_style = participant['perks']['styles'][1]
+    return sub_style['style']
+
+#게임 플레이날짜
+def get_time_ago_str(delta):
+    """
+    입력받은 timedelta 객체를 분 단위로 변환하여 문자열로 반환합니다.
+    """
+    delta_min = delta.total_seconds() // 60
+    if delta_min < 60:
+        return f"{int(delta_min)}분 전"
+    elif delta_min < 1440:
+        return f"{int(delta_min//60)}시간 전"
+    else:
+        return f"{int(delta_min//1440)}일 전"
+
+# 이미지 다운로드 함수
+def download_image(url, filename):
+    with open(filename, 'wb') as handle:
+        response = requests.get(url, stream=True)
+
+        if not response.ok:
+            print(response)
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            handle.write(block)
+
+#전적에서 사용된 챔피언 이미지 URL을 받아오는 함수
+def get_champion_image_url(champion_name):
+    version = get_version()
+    response = requests.get(f'https://ddragon.leagueoflegends.com/cdn/{version}/data/ko_KR/champion.json')
+    champions = response.json()['data']
+
+    for champ in champions.values():
+        if champ['id'] == champion_name:
+            url = f"http://ddragon.leagueoflegends.com/cdn/{version}/img/champion/{champ['image']['full']}"
+            return url
+
+#팀 전체 킬수를 가져오는 함수
+def get_team_kills(match_id, team_id):
+    match_data = get_match_data(match_id)
+    team_kills = 0
+    for participant in match_data["info"]["participants"]:
+        if participant["teamId"] == team_id:
+            team_kills += participant["kills"]
+    return team_kills
+
+#매치결과 반환
+def get_match_data(match_id):
+    url = f'https://asia.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={api_key}'
+    response = requests.get(url)
+    return response.json()
+
+#!롤전적 명령어 구현
+@bot.command(name="롤전적", aliases=['lol_Re'], help='사용법 *롤전적 [소환사명] [갯수] ')
+async def 롤전적(ctx, summoner_name, num):
+    try:
+        num = int(num)
+    except ValueError:
+        await ctx.send("전적의 개수는 숫자로 입력해주세요!")
+        return
+    
+    puuid = get_puuid(summoner_name)
+    if not puuid:
+        await ctx.send("소환사 정보를 가져올 수 없습니다. op.gg를 이용해주세요.")
+        return
+    
+    url = f'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={num}&api_key={api_key}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        match_ids = response.json()
+    else:
+        await ctx.send("전적 정보를 가져올 수 없습니다. op.gg를 이용해주세요.")
+        return
+
+    
+    for idx, match_id in enumerate(match_ids):
+        url = f'https://asia.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={api_key}'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            info = data['info']
+            participants = info['participants']
+
+            game_mode = get_game_mode(info['queueId'])
+            teamKills = 0
+            for participant in participants:
+                if participant['puuid'] == puuid:
+                    teamId = participant['teamId']
+                                
+                    for team in data['info']['teams']:
+                        if team['teamId'] == teamId:
+                            teamKills = team["objectives"]["champion"]["kills"]
+                    champ_name_e = participant['championName']
+                    champ_name_k = get_champion_name(champ_name_e.capitalize())
+                    game_duration = info['gameDuration']
+                    win = participant['win']
+                    kda = f"{participant['kills']} / {participant['deaths']} / {participant['assists']}"
+
+                    double_kill = participant['doubleKills']
+                    triple_kill = participant['tripleKills']
+                    quadra_kill = participant['quadraKills']
+                    penta_kill = participant['pentaKills']
+
+                    # match_info에서 챔피언 이미지를 받아옴
+                    champ_image_url = get_champion_image_url(champ_name_e.capitalize())
+
+                    # 이미지 파일 객체를 만듦
+                    response = requests.get(champ_image_url)
+                    #image_file = File(io.BytesIO(response.content), filename="champion.png")
+
+                    if participant['deaths'] == 0:
+                        kda_ratio = "Perfect"
+                    else:
+                        kda_ratio = round((participant['kills'] + participant['assists']) / participant['deaths'], 2)
+
+                    kill_involvement = (participant['kills'] + participant['assists'] ) / teamKills * 100
+
+                    cs = participant['totalMinionsKilled'] + participant['neutralMinionsKilled']
+                    cs_str = round(cs / (game_duration/60), 1)
+
+                    spells = [get_spell_name(participant['summoner1Id']), get_spell_name(participant['summoner2Id'])]
+
+                    '''primary_perk = get_rune_name(get_primary_perk(participant))
+                    sub_perk_style = get_rune_name(get_sub_perk_style(participant))'''
+
+                    primary_perk = get_primary_perk(participant)
+                    sub_perk_style = get_sub_perk_style(participant)
+
+                    '''try:
+                        tier = f"{data['tier']} {data['rank']}"
+                    except KeyError:
+                        tier = "Unranked"'''
+
+                    min = game_duration // 60
+                    sec = game_duration % 60
+
+                    time_ago = datetime.datetime.utcnow() - datetime.datetime.fromtimestamp(info['gameCreation']/1000.0)
+                    time_ago_str = get_time_ago_str(time_ago)
+                    
+                    match_info = (f"{game_mode} {'승리' if win else '패배'} {min}분 {sec}초 \n "
+                                f"{champ_name_k}  {kda} \t\t\t 평점 {kda_ratio}:1\n"
+                                f" \t\t\t 킬관여 {kill_involvement:,.1f}% CS{cs} ({cs_str})\n"
+                                f"더블킬:{double_kill} 트리플킬: {triple_kill} 쿼드라킬: {quadra_kill}, 펜타킬: {penta_kill}")
+                    
+                    embed = discord.Embed(title=f"{summoner_name}님의 최근 {num}판 전적", color=random.randint(0, 0xffffff))
+                    embed.add_field(name=f"**Match {idx+1}**", value=match_info, inline=False)
+                    embed.set_thumbnail(url=champ_image_url)
+                    await ctx.send(embed=embed)
+                    break
+
+    '''if match_list:
+            
+    else:
+        await ctx.send("전적 정보를 가져올 수 없습니다. op.gg를 이용해주세요.")'''
+        
+
+#버전 정보 
+def get_version():
+    response = requests.get("https://ddragon.leagueoflegends.com/api/versions.json")
+    if response.status_code == 200:
+        versions = response.json()
+        return versions[0]
+    
+#=============================================================
+
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
     	await ctx.send("명령어를 찾지 못했습니다")
+
+#=============================================================
 
 client.run("MTA4NTQwNTU4NTI2NjIwMDYzNw.G_R0BJ.VfPqMTAAJWMIQLbQwS8iebRMupgmxgO0N95FYQ")
