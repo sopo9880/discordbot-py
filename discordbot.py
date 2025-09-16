82c262d9e29808acab7411ce0d8ffac6bd47db78
sopo9880 <66225600+sopo9880@users.noreply.github.com>
2025-09-16 09:52:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,7 +18,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 # 기타 모듈
 from urllib import request
